2708114a0e37beb4d5b403a59639ef2bc672c38c
Julian Bäume <julian@svg4all.de>
2011-11-05 13:05:18 +0100

    implement all routes for core plugin
    these have mostly been copied over from fassets

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,38 @@
 FassetsCore::Engine.routes.draw do
+  resources :catalogs do
+    resources :facets do
+      resources :labels do
+        collection do
+          put :sort
+        end
+      end
+
+      put :add_asset
+    end
+  end
+
+  resources :classifications
+
+  resources :users do
+    resources :tray_positions do
+      collection do
+        put :replace
+      end
+    end
+  end
+
+  # assets
+  resources :urls
+  resources :file_assets do
+    member do
+      get :thumb
+      get :preview
+      get :original
+    end
+  end
+
+  match 'assets/:id/preview' => 'assets#preview'
+  match 'assets/:id/edit' => 'assets#edit'
+
+  root :to => "FassetsCore::Catalogs#index"
 end

15f952399dd2f7ff8e7e7061fbb45c831b7785fa
Julian Bäume <julian@svg4all.de>
2011-11-06 18:44:49 +0100

    fix helper method and shared asset view
    this now works as expected. kind of. ;)

--- app/helpers/fassets_core/assets_helper.rb
+++ app/helpers/fassets_core/assets_helper.rb
@@ -3,11 +3,14 @@ module FassetsCore
     def content_partial(content, partial)
       content.class.to_s.underscore.pluralize + "/" + content.media_type.to_s.underscore + "_" + partial.to_s 
     end
-    def asset_content_path(content, asset_id)
-      content.to_s.underscore.pluralize + "/" + asset_id.to_s
+
+    # construct the path for the assets content
+    def asset_content_path(content)
+      class_name = content.class.to_s.split("::").last
+      "#{root_path}#{class_name.underscore.pluralize}/#{content.id.to_s}"
     end
-    def edit_asset_content_path(content, asset_id)
-      content.underscore.pluralize + "/" + asset_id.to_s + "/edit"
+    def edit_asset_content_path(content)
+      asset_content_path(content) + "/edit"
     end
   end
 end

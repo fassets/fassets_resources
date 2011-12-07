1c86386b423f7160e5f461f2a385f995ee299922
Julian Bäume <julian@svg4all.de>
2011-12-07 15:43:09 +0100

    simplify javascript
    The active area is the complete div with .asset class. This includes the
    actions. If we enter the asset div, actions should be shown; if we leave
    it, it should be hidden.

--- app/assets/javascripts/fassets_core/assets.js
+++ app/assets/javascripts/fassets_core/assets.js
@@ -1,10 +1,6 @@
 $(document).ready(function(){
-  $(".asset_content").mouseenter(function(event){
-    $(".asset_actions").css("display","none");
-    $(this).parent().find(".asset_actions").show("slide", { direction: "left" }, 300);
-  });
-  $(".asset_actions").mouseleave(function(event){
-    $(this).find(".asset_actions").hide("slide", { direction: "left" }, 300);
+  $(".asset").mouseenter(function(event){
+    $(this).find(".asset_actions").show("slide", { direction: "left" }, 300);
   });
   $(".asset").mouseleave(function(event){
     $(this).find(".asset_actions").hide("slide", { direction: "left" }, 300);

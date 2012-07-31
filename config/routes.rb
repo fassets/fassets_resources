1d8993013b52bd7b089e4cebb2b1049995bbc46a
Julian Bäume <julian@svg4all.de>
2012-07-31 09:50:42 +0200

    update routes
    remove the fassets_core routes and move the Engine routes into own
    namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,4 @@
-Rails.application.routes.draw do
-  resources :catalogs do
-    resources :facets do
-      resources :labels do
-        collection do
-          put :sort
-        end
-      end
-    end
-    put :add_asset
-  end
-
-  resources :classifications
-
-  resources :users do
-    resources :tray_positions do
-      collection do
-        put :replace
-      end
-    end
-  end
-
+FassetsResources::Engine.routes.draw do
   # assets
   resources :urls
   resources :file_assets do
@@ -32,18 +11,8 @@ Rails.application.routes.draw do
 
   match 'file_assets/:id' => 'FileAssets#update'
   match 'urls/:id' => 'Urls#update'
-  match 'asset/:id/preview' => 'Assets#preview'
-  match 'asset/:id/edit' => 'Assets#edit'
-  match 'asset/:id/classifications' => 'Assets#classifications'
-  match 'catalog_box' => 'Catalogs#catalog_box'
-  match 'box_content' => 'Catalogs#box_content'
-  match 'box_facet' => 'Catalogs#box_facet'
-  match 'edit_box/:id' => 'Assets#edit_box'
   match 'new_remote_file' => 'FileAssets#new_remote_file'
   match 'get_wiki_imgs' => 'FileAssets#get_wiki_imgs'
   match 'search_wiki_imgs' => 'FileAssets#search_wiki_imgs'
   match 'add_wiki_img' => 'FileAssets#add_wiki_image'
-  match 'add_asset_box' => 'Assets#add_asset_box'
-
-  root :to => "Catalogs#index"
 end

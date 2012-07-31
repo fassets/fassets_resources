9830305ae09e0fecdf13b7f96e16a80245aec9eb
Julian Bäume <julian@svg4all.de>
2012-07-31 10:04:42 +0200

    remove fassets_core models
    these are not needed in this plugin

--- app/models/classification.rb
+++
@@ -1,8 +0,0 @@
-class Classification < ActiveRecord::Base
-  belongs_to :catalog
-  has_many :labelings, :dependent => :destroy
-  has_many :labels, :through => :labelings
-  belongs_to :asset, :counter_cache => true
-  validates_presence_of :asset
-end
-
--- app/models/labeling.rb
+++
@@ -1,5 +0,0 @@
-class Labeling < ActiveRecord::Base
-  belongs_to :label
-  belongs_to :classification
-end
-

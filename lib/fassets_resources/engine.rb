9ba9bb189ba861c46b6f1bbccabf21d20966f94a
Julian Bäume <julian@svg4all.de>
2012-07-31 10:54:46 +0200

    isolate namespace of resources engine

--- lib/fassets_resources/engine.rb
+++ lib/fassets_resources/engine.rb
@@ -1,5 +1,6 @@
 module FassetsResources
   class Engine < Rails::Engine
+    isolate_namespace FassetsResources
     initializer :assets do |config|
       Rails.application.config.assets.precompile += %w( flowplayer.js form.js jquery.collapsiblePanel-0.2.0.js jquery.fileupload.js jquery.fileupload-ui.js jquery.iframe-transport.js jquery.tmpl.min.js jquery.ui.widget.js jquery.fileupload-ui.css jquery-ui-theme_base.css)
     end

f578207ff49a9e26d95a6a4e9f3346159ab0a45b
Julian Bäume <julian@svg4all.de>
2012-09-10 17:29:06 +0200

    add migration to add fassets_resources namespace
    this is a mountable engine and therefore namespaced

---
+++ db/migrate/20120910152612_add_namespace_to_tables.rb
@@ -0,0 +1,6 @@
+class AddNamespaceToTables < ActiveRecord::Migration
+  def change
+    rename_table :file_assets, :fassets_resources_file_assets
+    rename_table :urls, :fassets_resources_urls
+  end
+end

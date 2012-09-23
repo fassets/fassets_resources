6abcafc67f507e11ae47a64bd6ec53e51b87c973
Julian Bäume <julian@svg4all.de>
2012-09-23 23:36:34 +0200

    add more asset types
    FileAsset also handles wikipedia image search and remote files

--- config/initializers/register_asset_types.rb
+++ config/initializers/register_asset_types.rb
@@ -2,4 +2,6 @@ require 'fassets_core'
 
 FassetsCore::Plugins::register({:name => "Url", :class => FassetsResources::Url})
 FassetsCore::Plugins::register({:name => "Local File", :class => FassetsResources::FileAsset})
+FassetsCore::Plugins::register({:name => "Remote File", :class => FassetsResources::FileAsset})
+FassetsCore::Plugins::register({:name => "Wikipedia Image", :class => FassetsResources::FileAsset})
 

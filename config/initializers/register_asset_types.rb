4448d67cf58f7b9c0c931b79afaec4e7f12585f1
Julian Bäume <julian@svg4all.de>
2012-09-23 22:32:29 +0200

    update fassets_core dependency to official version
    switch the dependency to use the version published at rubygems.org

--- config/initializers/register_asset_types.rb
+++ config/initializers/register_asset_types.rb
@@ -1,3 +1,5 @@
+require 'fassets_core'
+
 FassetsCore::Plugins::register({:name => "Url", :class => FassetsResources::Url})
 FassetsCore::Plugins::register({:name => "Local File", :class => FassetsResources::FileAsset})
 
--- lib/fassets_resources.rb
+++ lib/fassets_resources.rb
@@ -1,5 +1,6 @@
 require "fassets_resources/engine"
 require "carrierwave"
+require "fassets_core"
 
 module FassetsResources
 end

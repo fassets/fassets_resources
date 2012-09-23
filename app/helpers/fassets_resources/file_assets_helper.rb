56b7200d6a06ade774ff4fd002830b8ad0e1e930
Julian Bäume <julian@svg4all.de>
2012-09-23 23:37:58 +0200

    render file-asset form partial dependend on type
    FileAsset handles 3 types of files. Each needs another form to be
    rendered during the new action.

--- app/helpers/fassets_resources/file_assets_helper.rb
+++ app/helpers/fassets_resources/file_assets_helper.rb
@@ -1,17 +1,28 @@
 module FassetsResources::FileAssetsHelper
 
+  def self.partial_for_type(type)
+    prefix = "fassets_resources/file_assets/"
+    if type == "Local File"
+      return prefix + "local_file_form"
+    elsif type == "Remote File"
+      return prefix + "remote_file_form"
+    elsif type == "Wikipedia Image"
+      return prefix + "wiki_img_form"
+    end
+  end
+
   def video_player(content)
     javascript_tag %Q<
       flowplayer('player_#{content.id}', '/swf/flowplayer.swf', {
         clip: {
           autoPlay: false
         },
-        plugins: { 
-            controls: { 
-                url: 'flowplayer.controls.swf', 
-                bottom:0, 
-                height:24, 
-                backgroundColor: '#bbbbbb', 
+        plugins: {
+            controls: {
+                url: 'flowplayer.controls.swf',
+                bottom:0,
+                height:24,
+                backgroundColor: '#bbbbbb',
                 backgroundGradient: 'none',
                 timeColor: '#ffffff',
                 buttonColor: '#222222'

c06d37f247b0697929c3726f5a2cdf8297e5788e
Julian Bäume <julian@svg4all.de>
2012-09-26 19:37:27 +0200

    rename migration becauso of name clashes
    fassets_presentations ships a migration by this name

--- db/migrate/20120910152612_add_fassets_resources_namespace_to_tables.rb
+++ db/migrate/20120910152612_add_fassets_resources_namespace_to_tables.rb
@@ -1,4 +1,4 @@
-class AddNamespaceToTables < ActiveRecord::Migration
+class AddFassetsResourcesNamespaceToTables < ActiveRecord::Migration
   def change
     rename_table :file_assets, :fassets_resources_file_assets
     rename_table :urls, :fassets_resources_urls

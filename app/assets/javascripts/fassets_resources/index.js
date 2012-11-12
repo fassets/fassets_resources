41c3a2c183670e002da31a754540c44e3bc68054
Julian Bäume <julian@svg4all.de>
2012-11-12 12:45:20 +0100

    work on (rails-)assets for file_assets
    * remove jquery.fileupload stuff, this isn’t needed any longer
    * add some styling

--- app/assets/javascripts/fassets_resources/index.js
+++ app/assets/javascripts/fassets_resources/index.js
@@ -1,7 +1,5 @@
 //= require fassets_core
 //= require flowplayer
-//= require jquery.fileupload
-//= require jquery.fileupload-ui
 //= require_self
 //= require ./add_asset_box
 //= require ./wikipedia_search
